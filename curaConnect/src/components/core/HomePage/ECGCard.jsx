c674431473bd31339ab2367c3372699bb898b25d
Aryan Singhal <aryansinghalagra@gmail.com>
2026-02-06 15:22:22 +0530

    kk

--- curaConnect/src/components/core/HomePage/ECGCard.jsx
+++ curaConnect/src/components/core/HomePage/ECGCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../../../assets/Images/CuraConnectLogo.png";
+import logo from "../../../assets/Images/CuraConnectLogo.png";
 
 function ECGCard() {
   return (
