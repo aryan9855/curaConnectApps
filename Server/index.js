8718a8841de9cced398e9719dc9ab31d94afd318
Aryan Singhal <aryansinghalagra@gmail.com>
2026-02-06 15:33:47 +0530

    another commit

--- Server/index.js
+++ Server/index.js
@@ -29,7 +29,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite frontend
+    origin: process.env.NODE_ENV === "production"
+    ? process.env.FRONTEND_URL
+    : "http://localhost:5173", // Vite frontend
     credentials: true,               // allow cookies
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
